e30362edd944f03558dbd910bffb2d561ed5eb63
MISSIONX <luachia3@gmail.com>
2025-05-11 19:02:16 +0545

    main updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'fooderlich_theme.dart';
 
 void main() {
   // 1
@@ -10,20 +11,27 @@ class Fooderlich extends StatelessWidget {
   const Fooderlich({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // TODO: Create theme
+    final theme = FooderlichTheme.dark();
     // TODO: Apply Home widget
     // 3
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // TODO: Add theme
+      theme: theme,
       title: 'Fooderlich',
       // 4
       home: Scaffold(
-        // TODO: Style the title
-        appBar: AppBar(title: const Text('Fooderlich')),
+        appBar: AppBar(
+            title: Text(
+          'Fooderlich',
+          style: theme.textTheme.displayLarge,
+        )),
         // TODO: Style the body text
-        body: const Center(
-            child: Text('Let\'s get cooking 👩‍🍳 MfS here we go')),
+        body: Center(
+          child: Text(
+            'Let\'s get cooking 👩‍🍳 all mfs over here and therre',
+            style: theme.textTheme.displayLarge,
+          ),
+        ),
       ),
     );
   }
